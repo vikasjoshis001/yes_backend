d3f3a29689ffbda3cf8b7e894537d32a45196001
vikasjoshis001 <vikasjoshis001@gmail.com>
2021-04-04 09:09:13 +0530

    TransactionHistory table removed

--- Customers/admin.py
+++ Customers/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import CustomersModel, TransactionHistoryModel
+from .models import CustomersModel
 # Register your models here.
 
 admin.site.register(CustomersModel)
-admin.site.register(TransactionHistoryModel)
