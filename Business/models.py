02eded327ea7479802e27ba6ef81dc1187a50ee7
vikasjoshis001 <vikasjoshis001@gmail.com>
2021-04-04 09:08:17 +0530

    Folder creation with new business added

--- Business/models.py
+++ Business/models.py
@@ -8,6 +8,10 @@ class BusinessModel(models.Model):
     """ Business Database Table """
     businessId = models.AutoField(primary_key=True)
     businessName = models.CharField(max_length=200)
+    businessCreatedAt = models.DateTimeField(
+        auto_now_add=True, null=True, blank=True)
+    businessUpdatedAt = models.DateTimeField(
+        auto_now=True, null=True, blank=True)
 
     def __str__(self):
         return self.businessName
--- Business/views.py
+++ Business/views.py
@@ -1,7 +1,11 @@
+import os
+
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+
+from yes_backend.settings import sheetsFolderPath, sheetsFolder
 
 from requirements import success, error
 from .models import BusinessModel
@@ -18,25 +22,35 @@ class AddBusinessView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        businessName = request.data.get("businessName")
+        businessName = request.data.get('businessName')
         try:
+            path = sheetsFolderPath + "/" + sheetsFolder
+            if not os.path.exists(path):
+                path = os.path.join(sheetsFolderPath, sheetsFolder)
+                os.mkdir(path)
+            newPath = os.path.join(sheetsFolderPath, sheetsFolder)
             dic = {
                 "businessName": businessName
             }
             serializer = BusinessSerializer(data=dic)
             if (serializer.is_valid()):
                 serializer.save()
-            response_message = success.APIResponse(200, "Business Added Successfully", {
-                                                   "businessName": businessName}).respond()
+                myFolder = businessName
+                path = os.path.join(newPath, myFolder)
+                os.mkdir(path)
+                response_message = success.APIResponse(200, "Business Added Successfully", {
+                    "businessName": dic}).respond()
+            else:
+                response_message = error.APIResponse(404, "Unable to add Business due to invalid serializer", {
+                    'error': None}).respond()
         except Exception as e:
             response_message = error.APIResponse(404, "Unable to add Business", {
                                                  'error': str(e)}).respond()
         finally:
             return Response(response_message)
 
+
 # Get Business Api
-
-
 class GetBusinessView(generics.ListCreateAPIView):
     """Api for Getting Businesses"""
 
@@ -44,8 +58,8 @@ class GetBusinessView(generics.ListCreateAPIView):
 
     def get(self, request, **kwargs):
         try:
-            business_list = BusinessModel.objects.all()
-            serializer = BusinessSerializer(business_list, many=True)
+            businessList = BusinessModel.objects.all()
+            serializer = BusinessSerializer(businessList, many=True)
             response_message = success.APIResponse(200, "List of All Businesses", {
                                                    "businessList": serializer.data}).respond()
         except Exception as e:
@@ -72,12 +86,7 @@ class DeleteBusinessView(APIView):
                     200, "Business Deleted Successfully", None).respond()
             else:
                 response_message = error.APIResponse(
-                    404, "Authentication Failed", None).respond()
-
-        except BusinessModel.DoesNotExist:
-            response_message = error.APIResponse(
-                404, "Bussiness Does Not Exist", None).respond()
-
+                    404, "Invalid BusinessId", None).respond()
         except Exception as e:
             response_message = error.APIResponse(
                 404, "Invalid BusinessId", None).respond()
@@ -97,14 +106,20 @@ class EditBusinessView(generics.CreateAPIView):
             businessId = request.data.get("businessId")
             businessName = request.data.get("businessName")
 
+            businessObjFolder = BusinessModel.objects.get(
+                businessId=businessId)
             businessObj = BusinessModel.objects.filter(businessId=businessId)
-            businessObj.update(businessName=businessName)
-            response_message = success.APIResponse(
-                200, "Business Updated Successfully", None).respond()
-        except BusinessModel.DoesNotExist:
-            response_message = error.APIResponse(
-                404, "Bussiness Does Not Exist", None).respond()
-
+            if (businessObj):
+                previousFolder = sheetsFolderPath+"/" + \
+                    sheetsFolder + "/" + businessObjFolder.businessName
+                businessObj.update(businessName=businessName)
+                renamedFolder = sheetsFolderPath+"/" + sheetsFolder + "/" + businessName
+                os.rename(previousFolder, renamedFolder)
+                response_message = success.APIResponse(
+                    200, "Business Updated Successfully", None).respond()
+            else:
+                response_message = error.APIResponse(
+                    404, "Invalid BusinessId", None).respond()
         except Exception as e:
             response_message = error.APIResponse(
                 404, "Invalid BusinessId", None).respond()
